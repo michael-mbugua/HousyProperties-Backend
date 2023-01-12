3a34cbb7e7095eaef662c9840a64591ab3c4193c
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2023-01-12 17:16:12 +0300

    made change in table string

--- db/migrate/20230112130011_create_tenants.rb
+++ db/migrate/20230112130011_create_tenants.rb
@@ -5,7 +5,7 @@ class CreateTenants < ActiveRecord::Migration[7.0]
       t.integer :house_no
       t.integer :phone_no
       t.string :unit_type
-      t.integer :date_in
+      t.string :date_in
       t.integer :balance_due
 
       t.timestamps
