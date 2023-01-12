a472d021724c95f0d544cb669929842766529057
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2023-01-12 16:45:57 +0300

    add validation to tenant

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,5 @@
 class Tenant < ApplicationRecord
+    ##validtion
+    validates :name, :house_no, :phone_no, :unit_type, :balance_due, presence: true
+    ## attributes :id, :name, :house_no, :phone_no, :unit_type, :balance_due
 end
