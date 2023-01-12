643e7d7fff8416ca772065ea8886f709823e3de2
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2023-01-12 16:55:34 +0300

    remove commented attributes

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,5 +1,4 @@
 class Tenant < ApplicationRecord
     ##validtion
     validates :name, :house_no, :phone_no, :unit_type, :balance_due, presence: true
-    ## attributes :id, :name, :house_no, :phone_no, :unit_type, :balance_due
 end
