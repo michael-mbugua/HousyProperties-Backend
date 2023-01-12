661cc18e2222c5270c9ba4889e9e951450abc217
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2023-01-12 15:47:57 +0300

    add property controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,35 @@
 class PropertiesController < ApplicationController
-  
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+      def index
+          properties = Property.all
+          render json: properties
+        end
+      
+        def show
+          property = Property.find(params[:id])
+          render json: property
+        end
+      
+        def create
+          property = Property.create(property_params)
+          render json: property, status: :created
+        end
+
+        def destroy
+          property=Property.find(params[:id])
+          property.destroy
+          head :no_content
+        end
+    
+
+        private
+
+        def render_not_found_response
+          render json: {error: "Property not Found"}, status: :not_found
+        end
+      
+        def property_params
+          params.permit(:name,:property_type, :unit_type, :location,:image, :amount)
+        end
+
 end
