43d87609c36ca2b12be52e3424e48576f6488fa9
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-13 17:10:48 +0300

    data associations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,7 @@
 class Property < ApplicationRecord
     ##validation
     validates :name, :property_type, :unit_type, :location, :image, :amount, presence: true
+    has_many :tenants
+    has_many :housecomplains
+    has_many :payments
 end
