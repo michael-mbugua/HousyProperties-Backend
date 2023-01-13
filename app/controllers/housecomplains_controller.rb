17d5a4c22050170113655e66386b9c6c0f29d716
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-13 17:09:08 +0300

    controller action for house complains

---
+++ app/controllers/housecomplains_controller.rb
@@ -0,0 +1,29 @@
+class HousecomplainsController < ApplicationController
+    def index
+        complain=Housecomplain.all
+        render json: complain
+    end
+    def create
+        complain=Housecomplain.create(complain_params)
+        render json: complain, status: :created
+    end
+    def show
+        complain=Housecomplain.find(params[:id])
+        render json: complain
+    end
+    def update
+        complain=Housecomplain.find(params[:id])
+        complain.update(complain_params)
+        render json: complain
+    end
+    def destroy
+        complain=Housecomplain.find(params[:id])
+        complain.destroy()
+        head :no_content
+    end
+    private
+    def complain_params
+        params.permit(:Name,:Phone,:HouseType,:Email,:complain,:property_id)
+        
+    end
+end
