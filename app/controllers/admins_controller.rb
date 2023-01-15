431a0d4470b1b10f2911d414821789e03665316d
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-15 06:23:11 +0300

    admin authentication

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,19 @@
 class AdminsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    skip_before_action :authorized,only: [:create,:index]
+
     def index
         admin=Admin.all
         render json: admin
     end
     def create
-        admin=Admin.create(admin_params)
-        render json: admin, status: :created
+        admin = Admin.create!(admin_params)
+        if admin.valid?
+            token= encode_token({admin_id: admin.id})
+            render json: {admin: AdminSerializer.new(admin),jwt: token },status: :created
+        else
+            render json: {error: "Failed to create Admin"}, status: :unprocessable_entity
+        end
     end
     def destroy
         admin=Admin.find(params[:id])
@@ -23,6 +31,6 @@ class AdminsController < ApplicationController
     end
     private
     def admin_params
-        params.permit(:Name,:Phone,:Password,:Email)
+        params.permit(:Name,:Phone,:password,:Email)
     end
 end
