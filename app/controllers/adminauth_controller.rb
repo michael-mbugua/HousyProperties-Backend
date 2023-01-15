d51f17b95fe5acec0e6b6efb0bf5aa84e0e3c81a
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-15 06:22:55 +0300

    add admin login controller action

---
+++ app/controllers/adminauth_controller.rb
@@ -0,0 +1,19 @@
+class AdminauthController < ApplicationController
+    skip_before_action :authorized, only: [:create,:index]
+
+    def create
+        admin = Admin.find_by(name: admin_login_params[:name])
+        if admin &.authenticate(admin_login_params[:password])
+            token= encode_token({admin_id: admin.id})
+            render json: {admin: adminSerializer.new(admin),jwt: token},status: :accepted
+        else
+            render json: { error: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
+
+    private
+    def admin_login_params
+        params.permit(:name,:password)
+    end
+end
