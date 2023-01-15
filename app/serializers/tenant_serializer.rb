f9c2043e578aeb4d0f283359f56eccf2b945fead
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-15 06:28:54 +0300

    remove house no

--- app/serializers/tenant_serializer.rb
+++ app/serializers/tenant_serializer.rb
@@ -1,3 +1,3 @@
 class TenantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :house_no, :phone_no, :unit_type, :date_in, :balance_due
+  attributes :id, :name, :phone_no, :unit_type, :date_in, :balance_due,:property_id
 end
