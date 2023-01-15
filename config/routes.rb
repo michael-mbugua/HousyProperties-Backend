9e4e73060cc178df94f580b0ef45b9f788c26cf0
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-15 06:29:25 +0300

    add custom routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :housecomplains
   resources :admins
   resources :payments
-  resources :tenants
-  resources :properties
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tenants 
+  resources :properties do
+    resources :tenants, only: [:show,:index]
+  end
+  post '/login', to: 'adminauth#create'
+  post '/admin/login',to: 'auth#create'
+  get '/profile', to: 'tenants#profile'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 
 end
