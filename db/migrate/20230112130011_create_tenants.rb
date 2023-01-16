9ab412bd0c2ad1317ee0f5d5b136df76a0a85b4c
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-16 16:58:11 +0300

    minor naming changes

--- db/migrate/20230112130011_create_tenants.rb
+++ db/migrate/20230112130011_create_tenants.rb
@@ -2,10 +2,13 @@ class CreateTenants < ActiveRecord::Migration[7.0]
   def change
     create_table :tenants do |t|
       t.string :name
+      t.string :last_name
+      t.string :email 
+      t.string :HouseType
       t.integer :phone_no
       t.string :unit_type
       t.string :date_in
-      t.integer :balance_due
+      t.integer :rent_payable
       t.text :password_digest
       t.integer :property_id
       t.timestamps
