07c480d6f60e193d7406e55ea67a133dfcfce7c3
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-16 16:57:54 +0300

    add property entities

--- db/migrate/20230112105339_create_properties.rb
+++ db/migrate/20230112105339_create_properties.rb
@@ -1,12 +1,16 @@
 class CreateProperties < ActiveRecord::Migration[7.0]
   def change
     create_table :properties do |t|
-      t.string :name
+      t.string :propertyName
       t.string :property_type
       t.string :unit_type
       t.string :location
       t.string :image
       t.integer :amount
+      t.boolean :wifi
+      t.boolean :shower
+      t.boolean :balcony
+      t.boolean :parking
 
       t.timestamps
     end
