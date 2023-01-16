0d70da1b6739ff31bc150ddc6fce940c7313597f
Ashley <ashleynjeri7@gmail.com>
2023-01-16 16:17:51 +0300

    Created a new controller for handling forgot password actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   resources :housecomplains
   resources :admins
   resources :payments
