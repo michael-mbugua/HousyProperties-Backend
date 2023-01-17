a062c7cea33202fb3a50325715fbff804c72e878
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-17 20:42:49 +0300

    minor changes

--- app/serializers/admin_serializer.rb
+++ app/serializers/admin_serializer.rb
@@ -1,3 +1,3 @@
 class AdminSerializer < ActiveModel::Serializer
-  attributes :id,:Name,:Phone,:Email,:password
+  attributes :id,:Name,:Phone,:Email
 end
--- app/serializers/tenant_serializer.rb
+++ app/serializers/tenant_serializer.rb
@@ -1,3 +1,3 @@
 class TenantSerializer < ActiveModel::Serializer
-  attributes :id,:name,:last_name, :phone_no,:email,:password,:property_id,:HouseType, :unit_type, :date_in,:rent_payable 
+  attributes :id,:name,:last_name, :phone_no,:email,:property_id,:HouseType, :unit_type, :date_in,:rent_payable 
 end
