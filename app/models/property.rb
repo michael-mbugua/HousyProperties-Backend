f53829620c572a9ed1673ac566c0ae443f925130
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-17 19:51:39 +0300

    add search to property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,17 @@
 class Property < ApplicationRecord
+    include PgSearch::Model 
     ##validation
     validates :propertyName,:property_type, :unit_type, :location,:image, :amount,:wifi,:shower,:parking,:balcony, presence: true
     has_many :tenants
     has_many :housecomplains
     has_many :payments
+
+    pg_search_scope :search_by_propertyName_and_propertyType,
+    against: %i[propertyName  property_type location unit_type],
+    using: {
+        tsearch: {
+            prefix: true
+        }
+    }
+
 end
