fa2f65e26fd7cb7869c193f568c33e48461d5e4b
michael-mbugua <michael.mbugua@student.moringaschool.com>
2023-01-18 13:36:13 +0300

    change login to use name

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
     skip_before_action :authorized, only: [:create,:index]
 
     def create
-        tenant = Tenant.find_by(name: tenant_login_params[:name])
+        tenant = Tenant.find_by(email: tenant_login_params[:email])
         if tenant &.authenticate(tenant_login_params[:password])
             token= encode_token({tenant_id: tenant.id})
             render json: {tenant: TenantSerializer.new(tenant),jwt: token},status: :accepted
@@ -14,6 +14,6 @@ class AuthController < ApplicationController
 
     private
     def tenant_login_params
-        params.permit(:name,:password)
+        params.permit(:email,:password)
     end
 end
